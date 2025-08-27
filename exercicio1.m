2b0c2b8292dacabb31f351d7b46042a6c24aef67
feablopes <151747850+feablopes@users.noreply.github.com>
2025-08-27 13:38:44 -0300

    Add files via upload

--- exercicio1.m
+++ exercicio1.m
@@ -3,48 +3,35 @@ function t = exercicio1(func,func_d,x0)
 % nao alterar: inicio
 es = 0.01;
 imax = 20;
-
 % nao alterar: fim
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% digite seu codigo aqui
 
-  maxit = 20;
-  tol   = 0.01;
+t = zeros(20, 1);
 
-  t = x0;
-  for k = 1:maxit
-    f  = func(t);
+t(1) = 0.1;
 
-    % derivada numérica central
-    h  = 1e-6 * max(1, abs(t));
-    df = (func(t + h) - func(t - h)) / (2*h);
+erro = zeros(length(t), 1);
 
-    if ~isfinite(df) || df == 0
-      tr = NaN;
-      return
-    end
+for ii = 1:length(t)-1
+  if ii ~= 1
+    erro(ii) = abs(t(ii) - t(ii-1))/t(ii);
+    if erro(ii) < 0.01
+      break
+    endif
+  endif
+  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii));
+endfor
+  last_index = find(t, 1, 'last');
+  resultado = t(last_index)
 
-    t_new = t - f/df;
+  erro = erro(ii)
 
-    if k > 1
-      err = abs((t_new - t) / max(1, abs(t_new)));
-      if err < tol
-        tr = t_new;
-        return
-      end
-    end
+  resultado_interacoes = ii
 
-    t = t_new;
-  end
-
-  tr = t;
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+t = 1
 
 endfunction
-
 
 
